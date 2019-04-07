380043dba1c86e4b758a0eaa8827781e60a2a470
irsan12 <irsanramdan23@gmail.com>
2019-04-07 10:41:56 +0700

    commit2

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -10,6 +10,7 @@
         <link href="https://fonts.googleapis.com/css?family=Nunito:200,600" rel="stylesheet">
 
         <!-- Styles -->
+        <!-- sad -->
         <style>
             html, body {
                 background-color: #DEB887;
